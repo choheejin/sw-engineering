a90babc000bb487bb9194796d4fa22315bfc43f6
choheejin <hjcho9510@gmail.com>
2022-11-30 16:53:12 +0900

    회원가입 수정

--- src/pages/auth-page/SignupPage.js
+++ src/pages/auth-page/SignupPage.js
@@ -4,14 +4,16 @@ import axios from "axios";
 function SignupPage() {
     const [userID, setUserID] = new useState('');
     const [password, setPassword] = new useState('');
-    const [confirmPw, setConfirmPw] = new useState('');
+    const [cusName, setName] = new useState('');
+    const [cusTel, setTel] = new useState('');
 
     const submitInfo = (e) => {
         const data = {
             'cusNo' : '1',
-            'cusName' : userID,
-            'cusTel' : userID,
-            'cusAddr' : userID,
+            'cusID' : userID,
+            'cusName' : cusName,
+            'cusTel' : cusTel,
+            'cusPw' : password,
         }
         axios.post('https://localhost:4000', data).then(response => {
                 console.log(response);
@@ -29,14 +31,19 @@ function SignupPage() {
                            name="id"
                            className="focus:outline-none border border-gray-300 rounded-sm py-1"
                            onChange={(e) => { setUserID(e.target.value) }}/>
+                    <input type="text"
+                           name="name"
+                           className="focus:outline-none border border-gray-300 rounded-sm py-1"
+                           onChange={(e) => { setName(e.target.value)}}/>
+                    <input type="tel"
+                           name="tel"
+                           className="focus:outline-none border border-gray-300 rounded-sm py-1"
+                           onChange={(e) => { setTel(e.target.value)}}/>
                     <input type="password"
                            name="password"
                            className="focus:outline-none border border-gray-300 rounded-sm py-1"
                            onChange={(e) => { setPassword(e.target.value)}}/>
-                    <input type="password"
-                           name="confirmPw"
-                           className="focus:outline-none border border-gray-300 rounded-sm py-1"
-                           onChange={(e) => { setPassword(e.target.value)}}/>
+
                     <button type="submit"
                             className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-1">회원가입 하기</button>
                     <div className="text-end text-sm">이미 회원이신가요? <a href="/login" className="text-blue-600 font-bold text-lg">로그인 하기</a></div>
